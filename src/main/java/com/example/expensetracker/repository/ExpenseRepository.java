5dc06457c6f0b33ccc3afd4292821ee2bfcce180
goldencat <codingwithgoldencat@gmail.com>
2024-07-28 19:52:33 +0530

    start project

--- src/main/java/com/example/expensetracker/repository/ExpenseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.expensetracker.repository;
-
-import com.example.expensetracker.model.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-}
